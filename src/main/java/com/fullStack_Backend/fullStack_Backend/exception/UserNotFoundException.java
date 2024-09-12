6b480463b1be290544ce94630e845294e04b9764
Dhanuka Iroshan <dhanukairoshan99@gmail.com>
2024-09-12 17:22:57 +0530

    custom exception Handlin

---
+++ src/main/java/com/fullStack_Backend/fullStack_Backend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.fullStack_Backend.fullStack_Backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(Long id){
+        super("Could not found the user with id " + id);
+    }
+}
